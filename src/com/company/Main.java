321804569191f7f7833cac823220da647e56de2e
Valentinkognito <thevalik_mail@ua.fm>
2016-02-18 16:55:26 +0200

    vse tri zadachi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -48,6 +48,8 @@ public class Main {
 //------------------------------------------------------------------------------------------
 
 
+        // Zadacha 1 ----------------------------------------------------------------------
+        System.out.println("Matrica C ");
         int[][] arrC = new int[3][3];
         Random rand = new Random();
         for (int i = 0; i < arrC.length; i++) {
@@ -56,15 +58,40 @@ public class Main {
                 System.out.print(arrC[i][j] + " ");
             }
             System.out.println(" ");
+
         }
         System.out.println(" ");
 
+
+        System.out.println("Matrica D ");
         int[][] arrD = new int[3][3];
         Random randD = new Random();
         for (int i = 0; i < arrD.length; i++) {
             for (int j = 0; j < arrD.length; j++) {
                 arrD[i][j] = randD.nextInt(100);
                 System.out.print(arrD[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println(" ");
+
+        System.out.println("Symma C+D ");
+        int[][] arrS = new int[3][3];
+        for (int i = 0; i < arrS.length; i++) {
+            for (int j = 0; j < arrS.length; j++) {
+                arrS[i][j] = arrC[i][j] + arrD[i][j];
+                System.out.print(arrS[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println(" ");
+
+        System.out.println("Umnojenie C*D ");
+        int[][] arrU = new int[3][3];
+        for (int i = 0; i < arrU.length; i++) {
+            for (int j = 0; j < arrU.length; j++) {
+                arrU[i][j] = arrC[i][j] * arrD[j][i];
+                System.out.print(arrU[i][j] + " ");
             }
             System.out.println(" ");
         }
